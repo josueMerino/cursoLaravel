36e66d317843703271afed4c55364163a2bc10ab
josueMerino <josue.merino@alumnos.uneatlantico.es>
2020-03-24 11:29:53 +0100

    Practicing with Controllers

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
-Route::get('/test', function(){
-    return view('test');
-});
+Route::get('/dashboard', 'DashboardController@index');
 
