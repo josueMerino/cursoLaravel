acc2e18266af20e37b8353c12ea702ac8eac5a24
josueMerino <josue.merino@alumnos.uneatlantico.es>
2020-03-25 14:12:38 +0100

    Now you can edit the reports

---
+++ resources/views/expenseReport/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.base')
+
+@section('content')
+
+    <div class="row">
+        <div class="col">
+            <h1>New Report</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <a class="btn btn-secondary" href="/expense_reports">Back</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <form action="/expense_reports" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="title">Title:</label>
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Type a title">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+
+@endsection
--- resources/views/expenseReport/index.blade.php
+++ resources/views/expenseReport/index.blade.php
@@ -2,17 +2,27 @@
 
 @section('content')
 
-<div class="row">
     <div class="row">
-        <h1>Reports</h1>
-        <table class="table">
-            @foreach ($expenseReports as $expenseReport)
-            <tr>
-            <td>{{ $expenseReport->title }}</td>
-            </tr>
-            @endforeach
-        </table>
+        <div class="col">
+            <h1>Reports</h1>
+        </div>
     </div>
-</div>
+    <div class="row">
+        <div class="col">
+            <a class="btn btn-primary" href="/expense_reports/create">Create a new report</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <table class="table">
+                @foreach ($expenseReports as $expenseReport)
+                <tr>
+                <td>{{ $expenseReport->title }}</td>
+                <td><a href="/expense_reports/{{ $expenseReport->id }}/edit">Edit</a></td>
+                </tr>
+                @endforeach
+            </table>
+        </div>
+    </div>
 
 @endsection
