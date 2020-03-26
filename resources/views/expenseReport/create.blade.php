ec641504e94a381812ae43ed503802c69321a5f5
josueMerino <josue.merino@alumnos.uneatlantico.es>
2020-03-26 12:45:34 +0100

    Validation added to create and edit reports

--- resources/views/expenseReport/create.blade.php
+++ resources/views/expenseReport/create.blade.php
@@ -14,11 +14,20 @@
     </div>
     <div class="row">
         <div class="col">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             <form action="/expense_reports" method="post">
                 @csrf
                 <div class="form-group">
                     <label for="title">Title:</label>
-                    <input type="text" class="form-control" id="title" name="title" placeholder="Type a title">
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Type a title" value="{{ old('title') }}">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
--- resources/views/expenseReport/edit.blade.php
+++ resources/views/expenseReport/edit.blade.php
@@ -14,12 +14,21 @@
     </div>
     <div class="row">
         <div class="col">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         <form action="/expense_reports/{{ $report->id }}" method="post">
                 @csrf
                 @method('put')
                 <div class="form-group">
                     <label for="title">Title:</label>
-                    <input type="text" class="form-control" id="title" name="title" placeholder="Type a title">
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Edit the title" value="{{ old('title') }}">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
