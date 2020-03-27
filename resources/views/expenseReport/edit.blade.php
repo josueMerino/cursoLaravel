2cfea557dae2a87581a95d8e2ea09c4257dc399f
vagrant <vagrant@homestead>
2020-03-27 09:24:48 +0000

    Begin to create reports of what were the expenses

--- resources/views/expenseReport/edit.blade.php
+++ resources/views/expenseReport/edit.blade.php
@@ -4,7 +4,7 @@
 
     <div class="row">
         <div class="col">
-        <h1>Edit Report {{ $report->id }}</h1>
+        <h1>Edit Report "{{ $report->title }}"</h1>
         </div>
     </div>
     <div class="row">
